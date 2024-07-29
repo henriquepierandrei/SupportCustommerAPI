389ff187dae8d73d0fee5db5675b3bdb3893a802
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-07-29 08:59:58 -0300

    Initial commit

---
+++ SupportCustommerAPI/src/main/java/com/API_SupportCustommer/SupportCustommerAPI/SupportCustommerApiApplication.java
@@ -0,0 +1,13 @@
+package com.API_SupportCustommer.SupportCustommerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportCustommerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportCustommerApiApplication.class, args);
+	}
+
+}
---
+++ SupportCustommerAPI/src/test/java/com/API_SupportCustommer/SupportCustommerAPI/SupportCustommerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API_SupportCustommer.SupportCustommerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportCustommerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
