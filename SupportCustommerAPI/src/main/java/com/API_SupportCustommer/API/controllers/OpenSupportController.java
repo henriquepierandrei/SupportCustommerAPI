50826fc8d50042aaea76624da2b95df971b8e290
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-07-30 09:01:50 -0300

    update

--- SupportCustommerAPI/src/main/java/com/API_SupportCustommer/API/controllers/OpenSupportController.java
+++ SupportCustommerAPI/src/main/java/com/API_SupportCustommer/API/controllers/OpenSupportController.java
@@ -2,10 +2,12 @@ package com.API_SupportCustommer.API.controllers;
 
 import com.API_SupportCustommer.API.dto.SupportDto;
 import com.API_SupportCustommer.API.model.SupportModel;
+import com.API_SupportCustommer.API.model.UserModel;
 import com.API_SupportCustommer.API.repository.SupportRepository;
 import com.API_SupportCustommer.API.service.SupportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -22,21 +24,31 @@ public class OpenSupportController {
 
 
     @PostMapping("/create")
-    public ResponseEntity createSupport(@RequestBody SupportDto supportDto, SupportModel supportModel2){
-        Optional<SupportModel> supportModel = this.supportRepository.findByTicket(supportModel2.getTicket());
-        if (supportModel.isEmpty()){
-            SupportModel newSupport = new SupportModel();
-            newSupport.setDateSupport(new Date());
-            newSupport.setTitle(supportDto.title());
-            newSupport.setContent(supportDto.content());
-            newSupport.setTypeOfProblem(supportDto.type());
-            newSupport.setTicket(supportService.createTicket());
-            System.out.println(supportService.createTicket());
-            this.supportRepository.save(newSupport);
-            return ResponseEntity.ok("Support Created");
-        }
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity createSupport(@RequestBody SupportDto supportDto, @AuthenticationPrincipal UserModel user) {
+        String ticket;
+        Optional<SupportModel> existingSupport;
+
+        // Loop para garantir que o ticket gerado seja único
+        do {
+            ticket = supportService.createTicket();
+            existingSupport = supportRepository.findByTicket(ticket);
+        } while (existingSupport.isPresent());
+
+        SupportModel newSupport = new SupportModel();
+        newSupport.setDateSupport(new Date());
+        newSupport.setTitle(supportDto.title());
+        newSupport.setContent(supportDto.content());
+        newSupport.setDateSupport(new Date());
+        newSupport.setTypeOfProblem(supportDto.type());
+        newSupport.setTicket(ticket);
+
+        user.setQuantityTickets(user.getQuantityTickets() + 1);
+
+        supportRepository.save(newSupport);
+
+        return ResponseEntity.ok("Support Created");
     }
+
 
 
 }
